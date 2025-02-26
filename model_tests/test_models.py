903fa93b2a448707fed9e7cd3e1ffa8db5b82f27
Niel Ok <nielok@stanford.edu>
2025-02-25 23:37:08 -0800

    added analysis of eval data loss

--- model_tests/test_models.py
+++ model_tests/test_models.py
@@ -1,8 +1,13 @@
+''' 
+Metrics-based testing of models trained on MNIST dataset using DDPM.
+'''
+
 import os
 import sys
 import json
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 MODEL_TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 REPO_DIR = os.path.dirname(MODEL_TEST_DIR)
@@ -14,14 +19,7 @@ from train_mnist import create_mnist_dataloaders
 from model import MNISTDiffusion
 from utils import ExponentialMovingAverage
 
-def test_models(
-        min_epoch = 1, # Minimum epoch number to start testing from
-        device = "cpu", # Device to run model on
-        results_filename = "results.json" # File to save results to
-        ):
-    # Load data
-    train_loader, test_loader = create_mnist_dataloaders(batch_size=128,image_size=28)
-
+def get_model_paths():
     # Get model filepaths
     epoch_numbers = []
     model_paths = []
@@ -34,6 +32,20 @@ def test_models(
     # Sort model filepaths by epoch number
     sorted_model_paths = [f for _, f in sorted(zip(epoch_numbers, model_paths))]
     sorted_epoch_numbers = sorted(epoch_numbers)
+
+    return sorted_model_paths, sorted_epoch_numbers
+
+# Test models just based on the loss and ema loss
+def test_models_loss(
+        min_epoch=1, # Minimum epoch number to start testing from
+        device="cpu", # Device to run model on
+        results_filename="results.json" # File to save results to
+        ):
+    # Load data
+    train_loader, test_loader = create_mnist_dataloaders(batch_size=128,image_size=28)
+
+    # Get model filepaths
+    sorted_model_paths, sorted_epoch_numbers = get_model_paths()
 
     model_evals = {}
     for i in range(len(sorted_model_paths)):
@@ -81,17 +93,52 @@ def test_models(
         with open(results_filepath, 'w') as f:
             json.dump(model_evals, f)
 
-def analyze_eval_data(
+# Analyze eval data for loss 
+def analyze_eval_data_loss(
+        min_epoch=1, # Minimum epoch number to start analyzing from
         results_filename='results.json'
         ):
-    pass
     
+    # Load results
+    results_filepath = os.path.join(MODEL_TEST_DIR, 'test_results', results_filename)
+    with open(results_filepath, 'r') as f:
+        results = json.load(f)
+
+    mean_model_losses = []
+    mean_model_ema_losses = []
+    epoch_numbers = []
+    # Analyze results
+    for epoch_number, model_eval in results.items():
+        epoch_numbers.append(int(epoch_number))
+
+        model_losses = model_eval['model_losses']
+        model_ema_losses = model_eval['model_ema_losses']
+
+        average_model_loss = sum(model_losses) / len(model_losses)
+        average_model_ema_loss = sum(model_ema_losses) / len(model_ema_losses)
+        
+        mean_model_losses.append(average_model_loss)
+        mean_model_ema_losses.append(average_model_ema_loss)
+
+    # Graph results
+    plt.figure(figsize=(10, 5))
+    plt.plot(epoch_numbers, mean_model_losses, label='Model Loss')
+    plt.plot(epoch_numbers, mean_model_ema_losses, label='Model EMA Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    # Define custom tick positions (1, 5, 10, 15, 20, ...)
+    tick_positions = list(range(5, max(epoch_numbers) + 1, 5))
+    plt.xticks(tick_positions, rotation=90) 
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
-    min_epoch = 99
+    min_epoch = 1
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     results_filename = 'results.json'
     
     # Collect eval data
-    test_models(min_epoch=min_epoch, device=device, results_filename=results_filename)
+    #test_models_loss(min_epoch=min_epoch, device=device, results_filename=results_filename) # Comment out once data has been collected
 
     # Analyze eval data
+    analyze_eval_data_loss(min_epoch=min_epoch, results_filename=results_filename)
